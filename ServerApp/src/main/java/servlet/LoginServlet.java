8f2b8cf62a61baeaf4b76a96cda70a2a32a4defc
Javier Ardila <renderjavi1998@gmail.com>
2019-03-23 21:48:19 -0500

    Merge

--- ServerApp/src/main/java/servlet/LoginServlet.java
+++ ServerApp/src/main/java/servlet/LoginServlet.java
@@ -6,21 +6,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.vo.UserApp;
+import util.GsonPOJOFactory;
 
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
- 
-    public LoginServlet() {
-        super();
-    }
 
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public LoginServlet() {
+		super();
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("Login...");
+
+		UserApp user = GsonPOJOFactory.getPOJO(request.getReader(), UserApp.class);
+		/*SQLUserDAO sqlUser = new SQLUserDAO(conn.getStatement());
+		responseJson = new Gson().toJson(sqlUser.login(user.getTelefono(), user.getContraseña()));
+		response.getWriter().write(responseJson);
+		response.getWriter().flush();
+		System.out.println("logeado: " + responseJson);*/
 	}
+
+	// GET = obtener
+	// POST = crear / login
+	// PUT = actualizar o reemplazar
+	// DELETE = eliminar / logout
 }
